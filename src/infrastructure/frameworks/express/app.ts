0c1cc53f6484dbb8482f05575b47be4c97c3db37
Brubru <jordan.sankare@gmail.com>
2026-03-18 17:07:49 +0100

    add cors

--- src/infrastructure/frameworks/express/app.ts
+++ src/infrastructure/frameworks/express/app.ts
@@ -1,4 +1,5 @@
 import express, { Express } from 'express';
+import cors from 'cors';
 import { Composition } from './composition';
 import { authRouter } from './routes/auth.routes';
 import { errorMiddleware } from './middlewares/error.middleware';
@@ -6,6 +7,7 @@ import { errorMiddleware } from './middlewares/error.middleware';
 export function createApp({ authController, authGuard }: Composition): Express {
   const app = express();
 
+  app.use(cors());
   app.use(express.json());
 
   app.use('/auth', authRouter(authController));
